15efa88e59f62553ecc6ed8ad092e2a46707f223
Gustavo Homem <homem.gustavo@gmail.com>
2024-08-23 13:25:35 +0200

    pep8 compliance detail

--- src/app/views.py
+++ src/app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+
 @login_required
 def index(request):
     return render(request, 'index.html', locals())
-
