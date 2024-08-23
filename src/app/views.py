2ca80a9381139e1ec9821021f3175e2eadc66414
Gustavo Homem <homem.gustavo@gmail.com>
2024-08-23 13:20:56 +0200

    login screen

--- src/app/views.py
+++ src/app/views.py
@@ -1,9 +1,8 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.views.generic.base import TemplateView
+from django.shortcuts import render
 
 @login_required
 def index(request):
-    #return HttpResponse("Hello, world. This is Django.")
-    return TemplateView.as_view(template_name="index.html")
+    return render(request, 'index.html', locals())
 
