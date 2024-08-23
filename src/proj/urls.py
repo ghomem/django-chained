70729826fd231d30cd492dea4d9ca7a2951776b6
Gustavo Homem <homem.gustavo@gmail.com>
2024-08-23 12:05:09 +0200

    login screen

--- src/proj/urls.py
+++ src/proj/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("DJANGO_APPNAME/", include("DJANGO_APPNAME.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
 ]
