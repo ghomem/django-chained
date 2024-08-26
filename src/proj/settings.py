ee516e17901d509208d71617527f10160f5ae528
Gustavo Homem <homem.gustavo@gmail.com>
2024-08-26 10:54:59 +0200

    support prettifying the app

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -115,7 +115,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "BASE_DIR/static/"
+# inside the app folder
+STATIC_URL = "static/"
+
+# project wide
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
