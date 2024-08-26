9a6a222a79306d90cad35860c1277b28dc6bb984
Gustavo Homem <homem.gustavo@gmail.com>
2024-08-26 13:54:38 +0200

    support prettifying the app

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -62,7 +62,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'curated.context_processors.get_appname',
+                'djangoproject.context_processors.get_appname',
             ],
         },
     },
