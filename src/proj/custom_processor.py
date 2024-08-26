c99302ea6a4498aaa0c365d76cb1479bd5a49031
Gustavo Homem <homem.gustavo@gmail.com>
2024-08-26 16:14:02 +0200

    display name

--- src/proj/custom_processor.py
+++ src/proj/custom_processor.py
@@ -1,12 +1,8 @@
 from django.conf import settings
 
 
-def get_app_name(request):
+def get_app_information(request):
     return {
         'APP_NAME': settings.APP_NAME,
-    }
-
-def get_app_display_name(request):
-    return {
         'APP_DISPLAY_NAME': settings.APP_DISPLAY_NAME,
     }
