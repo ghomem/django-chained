1719181e831ce1108709277c5fa691f160641519
Gustavo Homem <homem.gustavo@gmail.com>
2024-09-02 21:52:30 +0000

    separate app logout from /admin logout

--- src/app/views.py
+++ src/app/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
+from django.conf import settings
+
+
+def app_logout(request):
+
+    logout(request)
+
+    return redirect(settings.APP_NAME)
 
 
 @login_required
